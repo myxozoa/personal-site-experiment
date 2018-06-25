d01ca9a3da65882a923e6211b344a2c92f0275e9
Ronnie Miksch <fakepreferences@gmail.com>
2018-06-25 15:06:31 -0700

    Inline svg ribbons
    - Added and cleaned up basic svg ribbons in the Ribbons.js component

--- src/components/Ribbons/Ribbons.js
+++ src/components/Ribbons/Ribbons.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Ribbons = () => {
   return (
     <React.Fragment>
-      <div></div>
-      <div></div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="-10 -100 799.298 650.682" width="1000px" height="auto">
+        <g className="cls-2" fill="#2a2a2a" transform="matrix(1, 0, 0, 1, 0, 0)">
+          <path id="Path_34-2" data-name="Path 34" className="cls-1" d="M-1307.743-1891h-251.065l781.3-632.681v203.306L-1307.742-1891Z" transform="translate(1567.81 2529.68)" />
+        </g>
+        <g class="cls-2" fill="#2a2a2a" transform="matrix(1, 0, 0, 1, 0, 0)">
+          <path id="Path_35-2" data-name="Path 35" class="cls-1" d="M-1753.936-2150H-2005l389.3-315.247v203.308L-1753.935-2150Z" transform="translate(2514 2701.25)" />
+        </g>
+      </svg>
     </React.Fragment>
   );
 };
